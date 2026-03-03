2eda2711fa33c1782388dd097f98e10371fcc25f
HuynhThuong <admin@huynhthuong.online>
2026-03-03 21:16:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,14 @@
 const express = require("express");
-const http = require("http");
-const WebSocket = require("ws");
-const pty = require("node-pty");
-const path = require("path");
-
 const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/terminal.html"));
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-wss.on("connection", (ws) => {
-  const shell = "bash";
+const PORT = process.env.PORT || 3000;
 
-  const ptyProcess = pty.spawn(shell, [], {
-    cols: 80,
-    rows: 30,
-    cwd: process.env.HOME,
-    env: process.env
-  });
-
-  ptyProcess.onData(data => ws.send(data));
-  ws.on("message", msg => ptyProcess.write(msg));
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
-
-server.listen(process.env.PORT || 3000);
