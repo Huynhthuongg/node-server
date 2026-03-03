91b4f27a29f417d647c42693f24f94325a25bb9a
HuynhThuong <admin@huynhthuong.online>
2026-03-03 20:45:51 +0700

    Update terminal.js

--- public/terminal.js
+++ public/terminal.js
@@ -1,15 +1,42 @@
-const socket = io();
+const input = document.getElementById("cmd");
+const output = document.getElementById("output");
 
-const term = new Terminal({
-  cursorBlink: true
-});
+function print(text) {
+  output.innerHTML += text + "\n";
+  window.scrollTo(0, document.body.scrollHeight);
+}
 
-term.open(document.getElementById("terminal"));
+print("LED Terminal Ready");
+print("Type help");
 
-socket.on("output", (data) => {
-  term.write(data);
-});
+input.addEventListener("keydown", function(e) {
+  if (e.key === "Enter") {
+    const cmd = input.value.trim();
+    print("root@server:~$ " + cmd);
 
-term.onData((data) => {
-  socket.emit("input", data);
+    if (cmd === "help") {
+      print("commands:");
+      print("status");
+      print("clear");
+      print("about");
+    }
+
+    else if (cmd === "status") {
+      print("Server running...");
+    }
+
+    else if (cmd === "about") {
+      print("AI Node System v1");
+    }
+
+    else if (cmd === "clear") {
+      output.innerHTML = "";
+    }
+
+    else {
+      print("command not found");
+    }
+
+    input.value = "";
+  }
 });
